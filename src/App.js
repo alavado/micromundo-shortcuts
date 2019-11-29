b7eb022d2196c960af2efe755756c620cf09a4fd
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-11-29 19:11:24 -0300

    iframe de prueba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Micromundo
+        <iframe src="https://micromundo.app" />
       </header>
     </div>
   );
